e51af5f208c490ffdd0fba1ea00e5e6f9af958a3
One-Lin <asd147267@gmail.com>
2025-02-24 17:20:21 +0800

    MatchPage 20250224 第一版MVC

--- SportMatch/Program.cs
+++ SportMatch/Program.cs
@@ -37,6 +37,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Match}/{action=MatchPage}/{id?}");
+    //pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
